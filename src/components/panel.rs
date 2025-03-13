af46a18a9b306b9d4613844d3b7c833302212983
mreich <reich@beontra.com>
2025-03-13 15:29:43 +0100

    basic number display with subtraction and correct enter input works

--- src/components/panel.rs
+++ src/components/panel.rs
@@ -1,17 +1,29 @@
+use std::cell::Ref;
 use std::num::ParseIntError;
+use dioxus::dioxus_core::internal::generational_box::GenerationalRef;
 use dioxus::prelude::*;
+use dioxus_logger::tracing;
 
+#[derive(Props, PartialEq, Clone)]
+struct CurrentScore {
+    remaining: u16,
+    thrown: u16
+}
 
 #[component]
 pub fn Panel() -> Element {
-    let init: Vec<u16> = vec![501];
-    let currentInput = use_signal(|| 1u16);
-    let mut count = use_signal(|| init);
+    let mut raw_input = use_signal(|| "".to_string());
+    let init_score = CurrentScore { remaining: 501, thrown: 0 };
+    let init_count_vector = vec![init_score];
+    let mut count = use_signal(|| init_count_vector);
+    let is_wrong = use_signal(|| false);
     rsx! {
         div { id: "numbers",
-            for a in count() {
+            for a in count.iter() {
                 div {
-                    {a.to_string()}
+                    {a.thrown.to_string()}
+                    "     "
+                    {a.remaining.to_string()}
                 }
             }
         }
@@ -20,18 +32,44 @@ pub fn Panel() -> Element {
         "Enter your latest result:",
         div {
             id: "panelDiv",
-            input {id: "numberField", type: "number", maxlength:10, min:0, onchange: move |event| input_changed(count, event)}
+            input {id: "numberField", type: "number", maxlength:10, min:0, oninput: move |e| raw_input.set(e.value()),
+                onkeypress: move |e| {
+                    if e.key() == Key::Enter {
+                        //tracing::info!("he11o");
+                        input_changed(count, is_wrong, raw_input)
+                    }
+                }
+            }
             button {id: "confirmButton", class: "bg-purple-200 px-4 py-2 rounded-lg border border-black hover:border-indigo-500 active:scale-95 transition-all", "Ok" }
+        }
+        div {
+            id: "displayError",
+            if is_wrong() {
+                "Please enter a valid number"
+            }
         }
     }
 }
 
-fn input_changed(mut count: Signal<Vec<u16>>, event: Event<FormData>) {
-    let result = event.value().parse();
+fn input_changed(mut count: Signal<Vec<CurrentScore>>, mut is_wrong: Signal<bool>, input_ref: Signal<String>) {
+    let result = input_ref.read().parse();
     match result {
         Ok(val) => {
-            count.write().push(val)
+            if val <= 180 {
+                let new_score = get_new_score(&count, val);
+                count.write().push(new_score);
+                is_wrong.set(false)
+            } else {
+                is_wrong.set(true)
+            }
         }
-        Err(_) => {}
+        Err(_) => { is_wrong.set(true)}
     }
+}
+
+fn get_new_score(count: &Signal<Vec<CurrentScore>>, val: u16) -> CurrentScore {
+    let generational_ref = count.read();
+    let last = generational_ref.last().unwrap();
+    let last_remaining = last.remaining;
+    CurrentScore { remaining: last_remaining - val, thrown: val }
 }
