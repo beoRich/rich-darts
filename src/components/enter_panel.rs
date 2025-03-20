f4931a05824bef214eece2e048ce7637306faaf0
mreich <reich@beontra.com>
2025-03-20 14:05:30 +0100

    restructure components

--- src/components/enter_panel.rs
+++ src/components/enter_panel.rs
@@ -12,56 +12,17 @@ pub fn EnterPanel(count: Signal<Vec<CurrentScore>>,
 
 ) -> Element {
    rsx! {
-
-
       div {
         id:"EnterPanel",
         class:"bg-base-100 shadow-md rounded px-8 pt-6 pb-8 mb-4 overflow-x-scroll",
-        NunberFieldError {count, raw_input, leg, error_message, score_message, allow_score}
-        div {
-            id: "ButtonsDiv",
-            class:"grid grid-cols-10 gap-4",
-
-                div {
-                    class:"col-span-1 grid ",
-                    button {id: "confirmButton",
-                        onclick: move |_| async move {
-                                input_wrapper(raw_input, leg, count, error_message, score_message).await;
-                        },
-                        disabled: if !allow_score() {true},
-                        class:"btn btn-soft btn-primary" , "Ok" },
-                }
-
-                div {
-                    class:"col-span-1 grid ",
-                    button {id: "undoButton",
-                        onclick: move |_| {
-                                undo_wrapper(count, error_message, score_message);
-                        },
-                        disabled: if count.read().len() < 2 {true},
-                        class:"btn btn-soft btn-secondary" , "Undo" },
-                }
-
-                div {
-                    class:"col-span-8 grid grid-cols-subgrid gap-4",
-                    div {
-                        class:"col-start-8",
-                        button {id: "newLegButton",
-                            onclick: move |_| async move {
-                                    new_leg_wrapper(leg, count, error_message, score_message).await;
-                            },
-                            class:"btn btn-soft btn-info" , "New Leg" },
-                    }
-                }
-        }
-
-
+        NumberFieldError {count, raw_input, leg, error_message, score_message, allow_score}
+        Buttons {count, raw_input, leg, error_message, score_message, allow_score}
     }
    }
 }
 
 #[component]
-fn NunberFieldError(
+fn NumberFieldError(
     count: Signal<Vec<CurrentScore>>,
     mut raw_input: Signal<String>,
     leg: Signal<u16>,
@@ -116,3 +77,57 @@ fn NunberFieldError(
     }
 
 }
+
+
+#[component]
+fn Buttons(
+    count: Signal<Vec<CurrentScore>>,
+    mut raw_input: Signal<String>,
+    leg: Signal<u16>,
+    mut error_message: Signal<ErrorMessageMode>,
+    score_message: Signal<ScoreMessageMode>,
+    allow_score: Signal<bool>
+) -> Element {
+
+    rsx! {
+
+        div {
+            id: "ButtonsDiv",
+            class:"grid grid-cols-10 gap-4",
+
+                div {
+                    class:"col-span-1 grid ",
+                    button {id: "confirmButton",
+                        onclick: move |_| async move {
+                                input_wrapper(raw_input, leg, count, error_message, score_message).await;
+                        },
+                        disabled: if !allow_score() {true},
+                        class:"btn btn-soft btn-primary" , "Ok" },
+                }
+
+                div {
+                    class:"col-span-1 grid ",
+                    button {id: "undoButton",
+                        onclick: move |_| {
+                                undo_wrapper(count, error_message, score_message);
+                        },
+                        disabled: if count.read().len() < 2 {true},
+                        class:"btn btn-soft btn-secondary" , "Undo" },
+                }
+
+                div {
+                    class:"col-span-8 grid grid-cols-subgrid gap-4",
+                    div {
+                        class:"col-start-8",
+                        button {id: "newLegButton",
+                            onclick: move |_| async move {
+                                    new_leg_wrapper(leg, count, error_message, score_message).await;
+                            },
+                            class:"btn btn-soft btn-info" , "New Leg" },
+                    }
+                }
+        }
+
+    }
+}
+
