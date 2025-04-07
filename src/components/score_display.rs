928f534debf8afacf4284fe33549c4e65dedcce4
mreich <reich@beontra.com>
2025-04-07 14:36:45 +0200

    recommendations up til 90

--- src/components/score_display.rs
+++ src/components/score_display.rs
@@ -1,8 +1,10 @@
-use crate::domain::Score;
 use crate::components::calculation::recommendation_calculation;
+use crate::domain::Score;
 use dioxus::prelude::*;
 #[component]
 pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
+    let remaining_column_width = "30";
+    let thrown_column_width = "15";
 
     rsx! {
         div {
@@ -13,26 +15,30 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                 class: "table-container",
                 table {
                     class: "text-xl uppercase bg-neutral-content rounded",
-                    style: "width: 65%",
+                    style: "width: 50%; border: 1px solid black;",
+                    border: "1px solid black;",
                     thead {
                         tr {
                             th {
                                 scope: "col",
-                                style: "white-space: pre; text-align: center;",
+                                style: "width: {remaining_column_width}%; white-space: pre; text-align: center;",
                                 class: "text-primary px-6 py-3",
+                                style: "border-right: 1px solid black; border-radius: 10px;",
                                 "Remaining"
                             }
                             th {
                                 scope: "col",
                                 style: "white-space: pre; text-align: center;",
+                                style: "border-right: 1px solid black; border-radius: 10px;",
                                 class: "text-secondary px-6 py-3",
-                                "Thrown"
+                                "Recommendation"
                             }
                             th {
                                 scope: "col",
-                                style: "white-space: pre; text-align: center;",
+                                style: "width: {thrown_column_width}%; white-space: pre; text-align: center;",
                                 class: "text-info px-6 py-3",
-                                "Recommendation"
+                                style: "border-right: 1px solid black; border-radius: 10px;",
+                                "Thrown"
                             }
                         }
                     }
@@ -45,7 +51,8 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                                     class: if i == 0 { "text-4xl bg-accent text-accent-content" },
                                     class: if i % 2 == 0 && i != 0 { "bg-base-200 text-base-content" },
                                     class: if i % 2 == 1 { "bg-base-300 text-base-content" },
-                                    style: "white-space: pre; text-align: center;",
+                                    style: "width: {remaining_column_width}%; white-space: pre; text-align: center;  ",
+                                    style: "border-right: 1px solid black; border-radius: 10px;",
                                     {format!("{:>3}", a.remaining.to_string())}
                                 }
                                 td {
@@ -54,20 +61,22 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                                     class: if i % 2 == 0 { "bg-base-200 text-base-content" },
                                     class: if i % 2 == 1 { "bg-base-300 text-base-content" },
                                     style: "white-space: pre; text-align: center;",
-                                    {format!("{:>3}", a.thrown.to_string())}
+                                    style: "border-right: 1px solid black; border-radius: 10px;",
+                                    {recommendation_calculation::basic(a.remaining).display().clone()}
                                 }
                                 td {
                                     class: "px-6 py-4",
                                     class: if i == 0 { "text-4xl bg-accent text-accent-content" },
                                     class: if i % 2 == 0 { "bg-base-200 text-base-content" },
                                     class: if i % 2 == 1 { "bg-base-300 text-base-content" },
-                                    style: "white-space: pre; text-align: center;",
-                                    {recommendation_calculation::basic(a.remaining).display().clone()}
+                                    style: "width: {thrown_column_width}%; white-space: pre; text-align: center;",
+                                    style: "border-right: 1px solid black; border-radius: 10px;",
+                                    {format!("{:>3}", a.thrown.to_string())}
                                 }
                             }
                         }
                     }
-                
+
                 }
             }
         }
