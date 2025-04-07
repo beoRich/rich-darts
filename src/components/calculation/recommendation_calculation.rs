31640078609e76a7886953255b560898f7a346c4
mreich <reich@beontra.com>
2025-04-07 17:11:27 +0200

    secondary recommendations up til 90

--- src/components/calculation/recommendation_calculation.rs
+++ src/components/calculation/recommendation_calculation.rs
@@ -1,112 +1,129 @@
-use crate::components::calculation::recommendation_calculation::IsFinish::Yes;
+use dioxus::core_macro::Props;
+use serde::{Deserialize, Serialize};
+use dioxus::prelude::*;
 
-pub struct RecValue {
-    is_finish: IsFinish,
-    rec_string: String,
+#[derive(PartialEq, Clone, Debug, Deserialize, Serialize)]
+pub enum RecValue {
+    IsFinish(FinishRecValue),
+    NoFinish(NonFinishRecValue)
+
 }
 
-impl RecValue {
-    pub fn display(&self) -> String {
-        match self {
-            RecValue {
-                is_finish: IsFinish::Yes,
-                rec_string,
-            } => rec_string.to_string(),
-            RecValue {
-                is_finish: IsFinish::No { goal },
-                rec_string,
-            } => format!("{rec_string} -> {goal}"),
-        }
-    }
+#[derive(Props, PartialEq, Clone, Debug, Deserialize, Serialize)]
+pub struct NonFinishRecValue {
+    pub rec: String,
+    pub goal: u16
 }
+
+#[derive(Props, PartialEq, Clone, Debug, Deserialize, Serialize)]
+pub struct FinishRecValue {
+    pub primary_rec: String,
+    pub secondary_rec: Option<String>,
+}
+
+
 
 enum IsFinish {
     Yes,
     No { goal: u16 },
 }
 
-pub fn basic(remaining_val: u16) -> RecValue {
+
+pub fn determine_rec(remaining_val: u16) -> RecValue {
     match remaining_val {
-        0 => RecValue{
-            is_finish: Yes,
-            rec_string: "-".to_string()
+        remaining_val if remaining_val < 91 => { RecValue::IsFinish(determine_finish_rec(remaining_val))}
+        remaining_val if remaining_val > 180 => {
+        let val = NonFinishRecValue{
+            goal: remaining_val - 180,
+            rec: "D20 D20 D20".to_string(),
+        };
+        RecValue::NoFinish(val)
         },
-        remaining_val if remaining_val <= 40 && remaining_val % 2 == 0 => RecValue {
-            is_finish: Yes,
-            rec_string: format!("D{}", remaining_val / 2),
-        },
-        remaining_val if remaining_val <= 40 && remaining_val % 2 == 1 => {
-            let (single, double) = under_40_two_dart_finish(remaining_val);
-            RecValue {
-            is_finish: Yes,
-            rec_string: format!("S{single} D{double}")}
-        },
-        remaining_val if remaining_val <= 56 && remaining_val > 40 => {
-            let (single, double) = (remaining_val - 32, 16);
-            RecValue {
-                is_finish: Yes,
-                rec_string: format!("S{single} D{double}"),
-            }
-        },
-        remaining_val if remaining_val <= 60 && remaining_val > 56  => {
-            let (single, double) = (remaining_val - 40, 20);
-            RecValue {
-                is_finish: Yes,
-                rec_string: format!("S{single} D{double}"),
-            }
-        },
-        61 => RecValue {is_finish: Yes, rec_string: "S25 D18".to_string()},
-        62 => RecValue {is_finish: Yes, rec_string: "T10 D16".to_string()},
-        63 => RecValue {is_finish: Yes, rec_string: "T13 D12".to_string()},
-        64 => RecValue {is_finish: Yes, rec_string: "T16 D8".to_string()},
-        65 => RecValue {is_finish: Yes, rec_string: "S25 D20".to_string()},
-        66 => RecValue {is_finish: Yes, rec_string: "T10 D18".to_string()},
-        67 => RecValue {is_finish: Yes, rec_string: "T9 D20".to_string()},
-        68 => RecValue {is_finish: Yes, rec_string: "T20 D4".to_string()},
-        69 => RecValue {is_finish: Yes, rec_string: "T11 D18".to_string()},
-        70 => RecValue {is_finish: Yes, rec_string: "T18 D8".to_string()},
-        71 => RecValue {is_finish: Yes, rec_string: "T13 D16".to_string()},
-        72 => RecValue {is_finish: Yes, rec_string: "T20 D6".to_string()},
-        73 => RecValue {is_finish: Yes, rec_string: "T19 D8".to_string()},
-        74 => RecValue {is_finish: Yes, rec_string: "T14 D16".to_string()},
-        75 => RecValue {is_finish: Yes, rec_string: "T17 D12".to_string()},
-        76 => RecValue {is_finish: Yes, rec_string: "T16 D14".to_string()},
-        77 => RecValue {is_finish: Yes, rec_string: "T19 D10".to_string()},
-        78 => RecValue {is_finish: Yes, rec_string: "T18 D12".to_string()},
-        79 => RecValue {is_finish: Yes, rec_string: "T19 D11".to_string()},
-        80 => RecValue {is_finish: Yes, rec_string: "T20 D10".to_string()},
-        81 => RecValue {is_finish: Yes, rec_string: "T19 D12".to_string()},
-        82 => RecValue {is_finish: Yes, rec_string: "T14 D20".to_string()},
-        83 => RecValue {is_finish: Yes, rec_string: "T17 D16".to_string()},
-        84 => RecValue {is_finish: Yes, rec_string: "T20 D12".to_string()},
-        85 => RecValue {is_finish: Yes, rec_string: "T15 D20".to_string()},
-        86 => RecValue {is_finish: Yes, rec_string: "T18 D16".to_string()},
-        87 => RecValue {is_finish: Yes, rec_string: "T17 D18".to_string()},
-        88 => RecValue {is_finish: Yes, rec_string: "T20 D14".to_string()},
-        89 => RecValue {is_finish: Yes, rec_string: "T19 D16".to_string()},
-        90 => RecValue {is_finish: Yes, rec_string: "T20 D15".to_string()},
-        remaining_val if remaining_val > 180 => RecValue {
-            is_finish: IsFinish::No {
-                goal: remaining_val - 180,
-            },
-            rec_string: "D20 D20 D20".to_string(),
-        },
-        _ => RecValue {
-            is_finish: IsFinish::No {
+        _ => {
+            let val = NonFinishRecValue{
                 goal: 32,
-            },
-            rec_string: format!("{}", remaining_val - 32),
+                rec: format!("{}", remaining_val - 32),
+            };
+            RecValue::NoFinish(val)
         },
     }
 }
 
-fn under_40_two_dart_finish(remaining_val: u16) -> (u16, u16) {
+fn determine_finish_rec(remaining_val: u16) -> FinishRecValue {
+    match remaining_val {
+        0 => FinishRecValue {
+            primary_rec: "-".to_string(),
+            secondary_rec: None,
+        },
+        remaining_val if remaining_val <= 40 && remaining_val % 2 == 0 => FinishRecValue {
+            primary_rec: format!("D{}", remaining_val / 2),
+            secondary_rec: None,
+        },
+        remaining_val if remaining_val <= 40 && remaining_val % 2 == 1 => {
+            let ((single, double), secondary_ref) = under_40_two_dart_finish(remaining_val);
+            FinishRecValue {
+                primary_rec: format!("S{single} D{double}"),
+                secondary_rec: secondary_ref.map(|(single, double)| format!("S{single} D{double}")),
+            }
+        },
+        remaining_val if remaining_val <= 56 && remaining_val > 40 => {
+            let (single, double) = (remaining_val - 32, 16);
+            let (single_snd, double_snd) = (remaining_val - 40, 20);
+            FinishRecValue {
+                primary_rec: format!("S{single} D{double}"),
+                secondary_rec: Some(format!("S{single_snd} D{double_snd}"))
+            }
+        },
+        remaining_val if remaining_val <= 60 && remaining_val > 56  => {
+            let (single, double) = (remaining_val - 40, 20);
+            FinishRecValue {
+                primary_rec: format!("S{single} D{double}"),
+                secondary_rec: None
+            }
+        },
+        61 => FinishRecValue {primary_rec: "S25 D18".to_string(), secondary_rec: Some("T15 D8".to_string())},
+        62 => FinishRecValue {primary_rec: "T10 D16".to_string(), secondary_rec: Some("T14 D10".to_string())},
+        63 => FinishRecValue {primary_rec: "T13 D12".to_string(), secondary_rec: None},
+        64 => FinishRecValue {primary_rec: "T16 D8".to_string(), secondary_rec: Some("T8 D20".to_string())},
+        65 => FinishRecValue {primary_rec: "S25 D20".to_string(), secondary_rec: Some("T11 D16".to_string())},
+        66 => FinishRecValue {primary_rec: "T10 D18".to_string(), secondary_rec: Some("T18 D6".to_string())},
+        67 => FinishRecValue {primary_rec: "T9 D20".to_string(), secondary_rec: None},
+        68 => FinishRecValue {primary_rec: "T20 D4".to_string(), secondary_rec: Some("T16 D10".to_string())},
+        69 => FinishRecValue {primary_rec: "T11 D18".to_string(), secondary_rec: Some("T15 D12".to_string())},
+        70 => FinishRecValue {primary_rec: "T18 D8".to_string(), secondary_rec: Some("T10 D20".to_string())},
+        71 => FinishRecValue {primary_rec: "T13 D16".to_string(), secondary_rec: Some("T17 D10".to_string())},
+        72 => FinishRecValue {primary_rec: "T20 D6".to_string(), secondary_rec: Some("T12 D18".to_string())},
+        73 => FinishRecValue {primary_rec: "T19 D8".to_string(), secondary_rec: None},
+        74 => FinishRecValue {primary_rec: "T14 D16".to_string(), secondary_rec: Some("T16 D13".to_string())},
+        75 => FinishRecValue {primary_rec: "T17 D12".to_string(), secondary_rec: Some("S25 S50".to_string())},
+        76 => FinishRecValue {primary_rec: "T16 D14".to_string(), secondary_rec: None},
+        77 => FinishRecValue {primary_rec: "T19 D10".to_string(), secondary_rec: None},
+        78 => FinishRecValue {primary_rec: "T18 D12".to_string(), secondary_rec: None},
+        79 => FinishRecValue {primary_rec: "T19 D11".to_string(), secondary_rec: Some("T13 D20".to_string())},
+        80 => FinishRecValue {primary_rec: "T20 D10".to_string(), secondary_rec: Some("D20 D20".to_string())},
+        81 => FinishRecValue {primary_rec: "T19 D12".to_string(), secondary_rec: Some("T15 D18".to_string())},
+        82 => FinishRecValue {primary_rec: "T14 D20".to_string(), secondary_rec: Some("S50 D16".to_string())},
+        83 => FinishRecValue {primary_rec: "T17 D16".to_string(), secondary_rec: Some("S50 S18 D20".to_string())},
+        84 => FinishRecValue {primary_rec: "T20 D12".to_string(), secondary_rec: Some("T16 D18".to_string())},
+        85 => FinishRecValue {primary_rec: "T15 D20".to_string(), secondary_rec: Some("T19 D14".to_string())},
+        86 => FinishRecValue {primary_rec: "T18 D16".to_string(), secondary_rec: None},
+        87 => FinishRecValue {primary_rec: "T17 D18".to_string(), secondary_rec: None},
+        88 => FinishRecValue {primary_rec: "T20 D14".to_string(), secondary_rec: None},
+        89 => FinishRecValue {primary_rec: "T19 D16".to_string(), secondary_rec: None},
+        90 => FinishRecValue {primary_rec: "T20 D15".to_string(), secondary_rec: Some("S50 D20".to_string())},
+        _ => panic!("Todo: Unreachable state {remaining_val}")
+    }
+}
+
+fn under_40_two_dart_finish(remaining_val: u16) -> ((u16, u16), Option<(u16, u16)>) {
     match  remaining_val {
-        39|37|35|33 => (remaining_val - 32, 16),
-        remaining_val if remaining_val < 32 && remaining_val > 16 => (remaining_val - 16, 8),
-        remaining_val if remaining_val < 16 && remaining_val > 8 => (remaining_val - 8, 4),
-        remaining_val if remaining_val < 8 && remaining_val > 4 => (remaining_val - 4, 2),
-        3 => (1, 1),
-        _ => panic!("Unreachable state {remaining_val}")
+        3 => ((1, 1), None),
+        39|37|35|33 => ((remaining_val - 32, 16), Some((remaining_val - 20, 10))),
+        remaining_val if remaining_val < 8 && remaining_val > 4 => ((remaining_val - 4, 2), None),
+        remaining_val if remaining_val < 16 && remaining_val < 10 => ((remaining_val - 8, 4), None),
+        remaining_val if remaining_val < 16 && remaining_val > 10 => ((remaining_val - 8, 4), Some((remaining_val - 10, 5))),
+        remaining_val if remaining_val < 32 && remaining_val < 20 => ((remaining_val - 16, 8), Some((remaining_val - 10, 5))),
+        remaining_val if remaining_val < 32 && remaining_val > 20 => ((remaining_val - 16, 8), Some((remaining_val - 20, 10))),
+        _ => panic!("Todo: Unreachable state {remaining_val}")
     }
 }
--- src/components/score_display.rs
+++ src/components/score_display.rs
@@ -1,10 +1,10 @@
 use crate::components::calculation::recommendation_calculation;
 use crate::domain::Score;
 use dioxus::prelude::*;
+use crate::components::calculation::recommendation_calculation::{FinishRecValue, NonFinishRecValue, RecValue};
+
 #[component]
 pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
-    let remaining_column_width = "30";
-    let thrown_column_width = "15";
 
     rsx! {
         div {
@@ -16,12 +16,11 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                 table {
                     class: "text-xl uppercase bg-neutral-content rounded",
                     style: "width: 50%; border: 1px solid black;",
-                    border: "1px solid black;",
                     thead {
                         tr {
                             th {
                                 scope: "col",
-                                style: "width: {remaining_column_width}%; white-space: pre; text-align: center;",
+                                style: "width: 30%; white-space: pre; text-align: center;",
                                 class: "text-primary px-6 py-3",
                                 style: "border-right: 1px solid black; border-radius: 10px;",
                                 "Remaining"
@@ -29,13 +28,13 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                             th {
                                 scope: "col",
                                 style: "white-space: pre; text-align: center;",
-                                style: "border-right: 1px solid black; border-radius: 10px;",
+                                style: "width: 50%; border-right: 1px solid black; border-radius: 10px;",
                                 class: "text-secondary px-6 py-3",
                                 "Recommendation"
                             }
                             th {
                                 scope: "col",
-                                style: "width: {thrown_column_width}%; white-space: pre; text-align: center;",
+                                style: "white-space: pre; text-align: center;",
                                 class: "text-info px-6 py-3",
                                 style: "border-right: 1px solid black; border-radius: 10px;",
                                 "Thrown"
@@ -48,12 +47,23 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                             tr {
                                 td {
                                     class: "px-6 py-4",
-                                    class: if i == 0 { "text-4xl bg-accent text-accent-content" },
+                                    class: if i == 0 { "text-5xl bg-accent text-accent-content" },
                                     class: if i % 2 == 0 && i != 0 { "bg-base-200 text-base-content" },
                                     class: if i % 2 == 1 { "bg-base-300 text-base-content" },
-                                    style: "width: {remaining_column_width}%; white-space: pre; text-align: center;  ",
+                                    style: "width: 30%; white-space: pre; text-align: center;  ",
                                     style: "border-right: 1px solid black; border-radius: 10px;",
                                     {format!("{:>3}", a.remaining.to_string())}
+                                }
+                                td {
+                                    key: {a.order},
+                                    class: "px-6 py-4",
+                                    class: if i == 0 { "text-3xl bg-accent text-accent-content" },
+                                    class: if i % 2 == 0 { "bg-base-200 text-base-content" },
+                                    class: if i % 2 == 1 { "bg-base-300 text-base-content" },
+                                    style: "width: 50%; white-space: pre; text-align: center;",
+                                    style: "border-right: 1px solid black; border-radius: 10px;",
+                                    RecommendationRow {rec_value: recommendation_calculation::determine_rec(a.remaining), row_nr: i as u16}
+
                                 }
                                 td {
                                     class: "px-6 py-4",
@@ -61,15 +71,6 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
                                     class: if i % 2 == 0 { "bg-base-200 text-base-content" },
                                     class: if i % 2 == 1 { "bg-base-300 text-base-content" },
                                     style: "white-space: pre; text-align: center;",
-                                    style: "border-right: 1px solid black; border-radius: 10px;",
-                                    {recommendation_calculation::basic(a.remaining).display().clone()}
-                                }
-                                td {
-                                    class: "px-6 py-4",
-                                    class: if i == 0 { "text-4xl bg-accent text-accent-content" },
-                                    class: if i % 2 == 0 { "bg-base-200 text-base-content" },
-                                    class: if i % 2 == 1 { "bg-base-300 text-base-content" },
-                                    style: "width: {thrown_column_width}%; white-space: pre; text-align: center;",
                                     style: "border-right: 1px solid black; border-radius: 10px;",
                                     {format!("{:>3}", a.thrown.to_string())}
                                 }
@@ -79,6 +80,74 @@ pub fn ScoreDisplay(scores: Signal<Vec<Score>>) -> Element {
 
                 }
             }
+        }
+    }
+}
+
+#[component]
+fn RecommendationRow(rec_value: RecValue, row_nr: u16) -> Element {
+    match rec_value {
+        RecValue::IsFinish(finish_rec_value) => {
+            rsx! {
+                FinishRecommendationRow{finish_rec_value, row_nr}
+            }
+        }
+        RecValue::NoFinish(no_finished_rec_value) => {
+            rsx! {
+                NoFinishedRecommendationRow{no_finished_rec_value, row_nr}
+            }
+        }
+    }
+}
+
+#[component]
+fn NoFinishedRecommendationRow(no_finished_rec_value: NonFinishRecValue, row_nr: u16) -> Element {
+    let NonFinishRecValue{rec, goal} = no_finished_rec_value;
+    rsx! {
+
+                                div {
+                                    div {
+                                        "{rec} -> {goal}"
+                                    }
+                                }
+
+    }
+}
+
+#[component]
+fn FinishRecommendationRow(finish_rec_value: FinishRecValue, row_nr: u16) -> Element {
+    let FinishRecValue {primary_rec, secondary_rec} = finish_rec_value;
+    rsx! {
+          if row_nr == 0  {
+                                    div {
+                                        class: "flex",
+                                        div {
+                                            class: "card bg-base-300 rounded-box grid h-16 grow place-items-center",
+                                            "{primary_rec}"
+                                        }
+                                        if secondary_rec.as_ref().is_some() {
+                                            div {
+                                                class: "divider divider-secondary divider-horizontal",
+                                            }
+                                            div {
+                                                class: "card bg-base-300 rounded-box grid h-16 grow place-items-center",
+                                                "{secondary_rec.as_ref().unwrap()}"
+                                            }
+
+                                        }
+
+                                    }
+
+                                }
+        if row_nr > 0 {
+                                        if secondary_rec.as_ref().is_none() {
+                                            {primary_rec.clone()}
+                                        }
+
+                                        if secondary_rec.as_ref().is_some() {
+                                            p {
+                                                "{primary_rec} | {secondary_rec.as_ref().unwrap()}"}
+                                        }
         }
     }
 }
