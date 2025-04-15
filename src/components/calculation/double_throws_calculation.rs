0416dfeb31b67e7519ddb2dc6fe59c04f2ed98e1
mreich <reich@beontra.com>
2025-04-15 14:55:34 +0200

    only ask for double above 50 if throw is zero

--- src/components/calculation/double_throws_calculation.rs
+++ src/components/calculation/double_throws_calculation.rs
@@ -14,36 +14,41 @@ pub enum HowManyOnDouble {
 pub fn ask_for_double_entry(last_score: &Score, new_score: &Score) -> HowManyOnDouble {
     if calculation::common::is_finish(last_score.remaining) {
         let mut double_attempt_vector: Vec<u16> = vec![0, 1, 2, 3];
-        if new_score.remaining == 0 {
-            double_attempt_vector.retain(|&x| x != 0)
+        if new_score.remaining > 50 && new_score.thrown != 0 {
+            NotRelevant
         }
-        let last_remaining = last_score.remaining;
-        if last_remaining > 110 {
-            double_attempt_vector.retain(|&x| x != 2 && x != 3)
-        }
-
-        if last_remaining >=99 && last_remaining < 110 {
-            let value = calculation::recommendation_calculation::determine_rec(last_remaining);
-            match value {
-                RecValue::IsFinish(FinishRecValue{ primary_rec, secondary_rec:_ }) => {
-                    match primary_rec {
-                        Some(val) => {if val.len() == 3  {double_attempt_vector.retain(|&x| x!=3)}}
-                        None => {panic!("Must have a primary recommendation")}
-                    }
-                }
-                _=> {panic!("Missing recommendation")}
+        else {
+            if new_score.remaining == 0 {
+                double_attempt_vector.retain(|&x| x != 0)
             }
-        }
+            let last_remaining = last_score.remaining;
+            if last_remaining > 110 {
+                double_attempt_vector.retain(|&x| x != 2 && x != 3)
+            }
+
+            if last_remaining >= 99 && last_remaining < 110 {
+                let value = calculation::recommendation_calculation::determine_rec(last_remaining);
+                match value {
+                    RecValue::IsFinish(FinishRecValue { primary_rec, secondary_rec: _ }) => {
+                        match primary_rec {
+                            Some(val) => { if val.len() == 3 { double_attempt_vector.retain(|&x| x != 3) } }
+                            None => { panic!("Must have a primary recommendation") }
+                        }
+                    }
+                    _ => { panic!("Missing recommendation") }
+                }
+            }
 
 
-        if last_remaining % 2 == 1 || (last_remaining > 40 && last_remaining != 50) {
-            double_attempt_vector.retain(|&x| x != 3);
-        }
+            if last_remaining % 2 == 1 || (last_remaining > 40 && last_remaining != 50) {
+                double_attempt_vector.retain(|&x| x != 3);
+            }
 
-        if double_attempt_vector.len() == 1 {
-            return OnlyOne;
+            if double_attempt_vector.len() == 1 {
+                return OnlyOne;
+            }
+            NeedEntry(double_attempt_vector)
         }
-        NeedEntry(double_attempt_vector)
     } else {
         NotRelevant
     }
@@ -138,7 +143,7 @@ mod test {
     }
 
     #[test]
-    fn only_not_relevant_on_non_finishes() {
+    fn not_relevant_on_non_finishes() {
         let finishes: Vec<u16> = (2..501)
             .into_iter()
             .map(|s| s as u16)
@@ -147,6 +152,25 @@ mod test {
         let ask_entries: Vec<(u16, HowManyOnDouble)> = finishes
             .into_iter()
             .map(|val| (val, ask_for_double_entry(&helper(val, val), &helper(300, 20))))
+            .collect();
+        for (test_val, entry) in ask_entries.into_iter() {
+            match entry {
+                NotRelevant => {},
+                OnlyOne => {panic!("wrong ask_entry (OnlyOne) for {}", test_val);},
+                NeedEntry(rec_val) => {panic!("wrong ask_entry (needentry) for {} with {:?}", test_val, rec_val);}
+            }
+        }
+    }
+
+    #[test]
+    fn not_relevant_on_non_0_above_50() {
+        let finishes: Vec<u16> = (51..501)
+            .into_iter()
+            .map(|s| s as u16)
+            .collect();
+        let ask_entries: Vec<(u16, HowManyOnDouble)> = finishes
+            .into_iter()
+            .map(|val| (val, ask_for_double_entry(&helper(val+100, 20), &helper(val, 100))))
             .collect();
         for (test_val, entry) in ask_entries.into_iter() {
             match entry {
